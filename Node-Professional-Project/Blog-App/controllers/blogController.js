a8362fd58b6834f57bf15d3eda4cba31d4d5ebdc
siddhrajparmar1302 <145355072+siddhrajparmar1302@users.noreply.github.com>
2025-05-06 15:45:42 +0530

    Node

---
+++ Node-Professional-Project/Blog-App/controllers/blogController.js
@@ -0,0 +1,43 @@
+const Blog = require('../models/Blog');
+
+exports.createBlog = async (req, res) => {
+  try {
+    const newBlog = new Blog(req.body);
+    await newBlog.save();
+    res.status(201).json(newBlog);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.getAllBlogs = async (req, res) => {
+  const blogs = await Blog.find();
+  res.json(blogs);
+};
+
+exports.getBlogById = async (req, res) => {
+  try {
+    const blog = await Blog.findById(req.params.id);
+    res.json(blog);
+  } catch (error) {
+    res.status(404).json({ message: "Blog not found" });
+  }
+};
+
+exports.updateBlog = async (req, res) => {
+  try {
+    const blog = await Blog.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(blog);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    res.json({ message: "Blog deleted" });
+  } catch (error) {
+    res.status(404).json({ message: "Blog not found" });
+  }
+};
